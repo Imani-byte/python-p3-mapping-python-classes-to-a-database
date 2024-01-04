0cd4a5da70ef16754afca1e0ab0ece0c21171c2b
naisiae.kasura@gmail.com <imani.kasura@student.moringaschool.com>
2024-01-05 00:27:30 +0300

    completed task

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
-from config import CONN, CURSOR
-from song import Song
+from .config import CONN, CURSOR
+from .song import Song
 
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    Song.create_table()
+
+    hello = Song("Hello", "25")
+    hello.save()
+
+    despacito = Song("Despacito", "Vida")
+    despacito.save()
+
+    print(hello.id)
+    print(despacito.id)
--- lib/testing/class_test.py
+++ lib/testing/class_test.py
@@ -7,12 +7,12 @@ class TestClass:
     def test_creates_songs_table(self):
         '''has classmethod "create_table()" that creates a table "songs" if table does not exist.'''
         Song.create_table()
-        assert(CURSOR.execute("SELECT * FROM songs"))
+        assert CURSOR.execute("SELECT * FROM songs")
 
     def test_initializes_with_name_and_album(self):
         '''takes a name and album as __init__ arguments and saves them as instance attributes.'''
         song = Song("Hold On", "Born to Sing")
-        assert(song.name == "Hold On" and song.album == "Born to Sing")
+        assert song.name == "Hold On" and song.album == "Born to Sing"
 
     def test_saves_song_to_table(self):
         '''has instancemethod "save()" that saves a song to music.db.'''
@@ -27,7 +27,7 @@ class TestClass:
             'SELECT * FROM songs WHERE name=? AND album=?',
             ('Hold On', 'Born to Sing')
         ).fetchone()
-        assert(db_song[1] == song.name and db_song[2] == song.album)
+        assert db_song[1] == song.name and db_song[2] == song.album
 
     def test_creates_and_returns_song(self):
         '''has classmethod "create()" that creates a Song instance, saves it, and returns it.'''
@@ -41,4 +41,4 @@ class TestClass:
             ('Hold On', 'Born to Sing')
         ).fetchone()
 
-        assert(db_song[0] == song.id and db_song[1] == song.name and db_song[2] == song.album)
+        assert db_song[0] == song.id and db_song[1] == song.name and db_song[2] == song.album
